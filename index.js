10d72d30dd0fb7e3db609c605d6ddc545cf41ae2
llvovll89 <svvvs5579@naver.com>
2024-04-15 00:02:02 +0900

    closer

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const onRoute = (e) => {
-    e.preventDefault();
-    const path = e.target.pathname
-    window.history.pushState({}, "", path);
+// const onRoute = (e) => {
+//     e.preventDefault();
+//     const path = e.target.pathname
+//     window.history.pushState({}, "", path);
+// }
+
+// const routes = {
+//     FALL_BACK : "/pages/404.html",
+//     "/": "/pages/home.html",
+//     "/about": "/pages/about.html"
+// };
+
+// const onRouteChange = () => {
+//     const pathName = window.location.pathname;
+//     const route = routes[pathName];
+//     console.log(pathName, route);
+
+//     fetch(route).then(data => data.text()).then(html => document.getElementById("content").innerHTML = html);
+// };
+
+// document.querySelectorAll("a").forEach(el => {
+//     el.addEventListener("click", (e) => {
+//         onRoute(e);
+//         onRouteChange();
+//     })
+// })
+
+// window.onpopstate = onRouteChange;
+// onRouteChange();
+
+function fn(a = 10, b = 100) {
+    // 함수 스코프 안에서 여러번 호출되서 값이 변경됨
+    let addNum = 0;
+
+    function inFn() {
+        a += b;
+        addNum++;
+        console.log(addNum);
+    }
+
+    return inFn;
 }
 
-const routes = {
-    FALL_BACK : "/pages/404.html",
-    "/": "/pages/home.html",
-    "/about": "/pages/about.html"
-};
+// 클로저 
+const result = fn();
+result()
+result()
+result()
+result()
 
-const onRouteChange = () => {
-    const pathName = window.location.pathname;
-    const route = routes[pathName];
-    console.log(pathName, route);
-
-    fetch(route).then(data => data.text()).then(html => document.getElementById("content").innerHTML = html);
-};
-
-document.querySelectorAll("a").forEach(el => {
-    el.addEventListener("click", (e) => {
-        onRoute(e);
-        onRouteChange();
-    })
-})
-
-window.onpopstate = onRouteChange;
-onRouteChange();
+console.log(result());
