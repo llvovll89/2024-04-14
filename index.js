7a304de19939c7b92d0a08030e3b007e426c0481
llvovll89 <svvvs5579@naver.com>
2024-04-15 00:26:44 +0900

    spa 수정

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-// const onRoute = (e) => {
-//     e.preventDefault();
-//     const path = e.target.pathname
-//     window.history.pushState({}, "", path);
-// }
+const onRoute = (e) => {
+    e.preventDefault();
+    const path = e.target.pathname
+    window.history.pushState({}, "", path);
+}
 
-// const routes = {
-//     FALL_BACK : "/pages/404.html",
-//     "/": "/pages/home.html",
-//     "/about": "/pages/about.html"
-// };
+const routes = {
+    FALL_BACK : "/pages/404.html",
+    "/": "/pages/home.html",
+    "/about": "/pages/about.html"
+};
 
-// const onRouteChange = () => {
-//     const pathName = window.location.pathname;
-//     const route = routes[pathName];
-//     console.log(pathName, route);
+const onRouteChange = () => {
+    const pathName = window.location.pathname;
+    const route = routes[pathName];
+    console.log(pathName, route);
 
-//     fetch(route).then(data => data.text()).then(html => document.getElementById("content").innerHTML = html);
-// };
+    fetch(route).then(data => data.text()).then(html => document.getElementById("content").innerHTML = html);
+};
 
-// document.querySelectorAll("a").forEach(el => {
-//     el.addEventListener("click", (e) => {
-//         onRoute(e);
-//         onRouteChange();
-//     })
-// })
+document.querySelectorAll("a").forEach(el => {
+    el.addEventListener("click", (e) => {
+        onRoute(e);
+        onRouteChange();
+    })
+})
 
-// window.onpopstate = onRouteChange;
-// onRouteChange();
+window.onpopstate = onRouteChange;
+onRouteChange();
 
 function fn(a = 10, b = 100) {
     // 함수 스코프 안에서 여러번 호출되서 값이 변경됨
@@ -35,7 +35,7 @@ function fn(a = 10, b = 100) {
     function inFn() {
         a += b;
         addNum++;
-        console.log(addNum);
+        Swal.fire(addNum);
     }
 
     return inFn;
